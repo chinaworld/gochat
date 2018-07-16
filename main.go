d332d43ddcb6e536348b58da166112b08ffca53b
lishuyang <shu123green@qq.com>
2018-07-16 14:01:09 +0800

    增加异常捕获

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func main() {
 	tool.LogDebug, logfile = tool.NewLog()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	config := config.Config{}
+
+	defer func() {
+		if err := recover(); err != nil {
+			tool.LogDebug.Println(err) //这里的err其实就是panic传入的内容，55
+		}
+	}()
 	config.Port = 8888
 	config.Ip = net.ParseIP("localhost")
 	config.SetConfig()
